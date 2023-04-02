56bf3fb24289914c642bc0dafa90cf22d227777a
Huseni Sameer Jaker <103500196+sameerhuseni@users.noreply.github.com>
2023-04-02 13:34:55 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const SearchBar = (props) => {
-    const [searchInput, setSearchInput] = useState("");
-    
-    return (
-        <form>
-            <input type="text" className="search-input"/>
-            <button type="submit" className="search-btn">Search</button>
-        </form>
-    );
-}
+const SearchBar = ({ setMovies }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=${searchTerm}`)
+      .then(response => {
+        setMovies(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
 
 export default SearchBar;
